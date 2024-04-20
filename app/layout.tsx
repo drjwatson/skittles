509cac426ec1364345d59c713bbab067cdfe30e5
Josh Watson <joshw@knorrmarketing.com>
2024-04-20 15:31:17 -0400

    Added favicon and og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/img/favicon.ico" />
+      </head>
       <body className={inter.variable}>
         <Toaster position="bottom-center" toastOptions={{
             error: { duration: 3000, className: '!bg-rose-600 !text-white mb-5' },
